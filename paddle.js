796ca84e65ce0b22ef3c2828614821f55f9ca4bf
Andrea_Graziosi <andreaggraziosi@gmail.com>
2021-06-15 14:29:11 -0700

    class effort oop challenge

--- paddle.js
+++ paddle.js
@@ -1,21 +1,20 @@
-class drawPaddle {
-
-constructor(ctx) {
-        this.paddleHeight = 10;
-        this.paddleWidth = 100;
-        this.paddleX = (canvas.width-this.paddleWidth) / 2;  
+class Paddle {
+    constructor(canvas) {
+      this.paddleHeight = 10;
+      this.paddleWidth = 100;
+      this.paddleX = (canvas.width - this.paddleWidth) / 2;
+      // TODO add y
+      this.color = '#F08080'
     }
-
-
-   drawPaddle(ctx, canvas) {
-    ctx.beginPath();
-    ctx.rect(this.paddleX, 
-        canvas.height-this.paddleHeight, 
-        this.paddleWidth, 
+    drawPaddle(ctx, canvas) {
+      ctx.beginPath();
+      ctx.rect(this.paddleX,
+        canvas.height - this.paddleHeight,
+        this.paddleWidth,
         this.paddleHeight);
-    ctx.fillStyle = "#f08080";
-    ctx.fill();
-    ctx.closePath();
-}
-
-}
+      ctx.fillStyle = this.color;
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+  export default Paddle;
